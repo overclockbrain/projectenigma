c1d4e75d0486768d28fde98d7fa41ab8d247546f
hot brain <63366646+overclockbrain@users.noreply.github.com>
2025-12-23 11:48:55 +0900

    feat: KAN-40 ゲーム進行状況とユーザー情報のエンティティおよびリポジトリを追加

---
+++ src/main/java/com/example/cipher/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.cipher.repository;
+
+import com.example.cipher.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * ユーザー情報を操作するためのリポジトリインターフェース。
+ * JpaRepositoryを継承しており、基本的なCRUD操作が利用可能。
+ * @param <User> エンティティの型
+ * @param <String> エンティティのIDの型
+ * @author R.Morioka
+ * @version 1.0
+ * @since 1.0
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
