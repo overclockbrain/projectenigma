0ae112f43183bdfa632b8a0509dcc107ff17ddd9
hot brain <63366646+overclockbrain@users.noreply.github.com>
2026-01-08 14:42:53 +0000

    feat: KAN-49 ステージ2の人狼ゲームにおける容疑者の追加とUIの改善を実施

--- src/test/java/com/example/projectenigma/cipher/controller/RootControllerTest.java
+++ src/test/java/com/example/projectenigma/cipher/controller/RootControllerTest.java
@@ -15,8 +15,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Optional;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
